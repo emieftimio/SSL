39ad533cb6f9b1e34263f5315b600e7c5bfe2758
emieftimio <emieftimio@gmail.com>
2018-04-03 12:31:35 -0300

    Add files via upload

--- CHelloWorld/helloWorld.c
+++ CHelloWorld/helloWorld.c
@@ -8,10 +8,29 @@
  ============================================================================
  */
 
+// 	Trabajo #0
+//	Autor: Emiliano Eftimio
+// 	Curso: K2051 - Profesor:Sola
+//	Este programa imprime por consola el mensaje Hello World contenido en la variable salida
+// 	Ademas captura esta salida y la guarda en un archivo output.txt
+//	Programa realizado mediante el IDE de Eclipse Mars
+
 #include <stdio.h>
 
 
 int main(void) {
-	printf("Hello World!");
+	FILE *archivo;
+	char salida[30] = "Hello World!.\n";
+
+	printf(salida);	// Muestro variable salida de tipo char, por consola.
+
+	archivo= fopen("output.txt","w+");	// Abro archivo output.txt y lo almaceno en una variale archivo con permisos de escritura
+
+	fprintf(archivo,salida);	// Imprimo variable salida en archivo output.txt
+
+
+	fclose(archivo);		// Cierro archivo output.txt
+
 	return 0;
 }
+
